10d6a8f2318ef6e5ec0366fac83388e3554f07de
parangodev <pabloarangobte@gmail.com>
2024-08-23 12:10:42 -0600

    Edit constans and giscus

--- src/consts.ts
+++ src/consts.ts
@@ -1,40 +1,43 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "ToolTrace blog",
+  DESCRIPTION:
+    "En ToolTrace, registro mis aprendizajes y pruebas de nuevas herramientas y códigos, compartiendo mi progreso en el mundo del desarrollo.",
+  EMAIL: "pabloarangobte@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  TITLE: "Inicio",
+  DESCRIPTION:
+    "Una visión general de mi viaje en desarrollo, con herramientas, código, y progreso documentado.",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  DESCRIPTION:
+    "Una colección de mis aprendizajes sobre herramientas y tecnologías de desarrollo",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
+  TITLE: "Proyectos",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+    "Una colección de mis proyectos con enlaces a repositorios y demostraciones en vivo.",
 };
 
 export const SOCIALS: Socials = [
   {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
+    NAME: "X (Twitter)",
+    HREF: "https://x.com/parangodev",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/parangodev",
   },
   {
     NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
+    HREF: "https://www.linkedin.com/in/pabloarangodev/",
   },
 ];
