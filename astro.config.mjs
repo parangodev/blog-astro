a42b0ec1745c352928906070edebb67c0e2e4182
parangodev <pabloarangobte@gmail.com>
2024-08-25 15:12:42 -0600

    ADD new post and google analitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,25 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://parango.dev",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx(),
+    pagefind(),
+    partytown({
+      config: {
+        config: { debug: false },
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
